244c4b58cc88e9986d6ea2f57dbce91f2804ed7e
JavierCabreraQuesada <39194510+JavierCabreraQuesada@users.noreply.github.com>
2020-04-04 12:10:13 +0200

    Add files via upload
    Scripts de BBDD

---
+++ Evento.sql
@@ -0,0 +1,9 @@
+/*
+-- Query: SELECT * FROM tickets.evento
+-- Date: 2020-04-04 12:06
+*/
+INSERT INTO `evento` (`id_evento`,`id_recinto`,`descripcion`,`fecha`) VALUES ('1111111111','11111','Concierto Iron Maiden en Alcorcón','2020-04-05 20:00:00');
+INSERT INTO `evento` (`id_evento`,`id_recinto`,`descripcion`,`fecha`) VALUES ('1111111112','88888','Desfile de la Victoria en la Plaza Roja Moscú','2020-05-09 12:00:00');
+INSERT INTO `evento` (`id_evento`,`id_recinto`,`descripcion`,`fecha`) VALUES ('1111111113','99999','Exposición Arte en el MOMA','2020-03-12 10:00:00');
+INSERT INTO `evento` (`id_evento`,`id_recinto`,`descripcion`,`fecha`) VALUES ('1111111114','22222','Visita al Museo del Prado','2020-04-10 10:00:00');
+INSERT INTO `evento` (`id_evento`,`id_recinto`,`descripcion`,`fecha`) VALUES ('1111111115','33333','Visita a la Sagrada Familia','2020-04-08 11:00:00');
