2ba14c326514040693b36546fe7b9790945d3f7d
JavierCabreraQuesada <39194510+JavierCabreraQuesada@users.noreply.github.com>
2020-04-04 12:13:12 +0200

    Add files via upload
    Scripts de BBDD

---
+++ BBDD/Ticket.sql
@@ -0,0 +1,29 @@
+/*
+-- Query: SELECT * FROM tickets.ticket
+-- Date: 2020-04-04 12:06
+*/
+INSERT INTO `ticket` (`id_ticket`,`id_evento`,`num_entrada`,`estado`) VALUES ('11111111111111111111','1111111111',1,'v');
+INSERT INTO `ticket` (`id_ticket`,`id_evento`,`num_entrada`,`estado`) VALUES ('11111111111111111112','1111111111',2,'v');
+INSERT INTO `ticket` (`id_ticket`,`id_evento`,`num_entrada`,`estado`) VALUES ('11111111111111111113','1111111111',3,'v');
+INSERT INTO `ticket` (`id_ticket`,`id_evento`,`num_entrada`,`estado`) VALUES ('11111111111111111114','1111111111',4,'v');
+INSERT INTO `ticket` (`id_ticket`,`id_evento`,`num_entrada`,`estado`) VALUES ('11111111111111111115','1111111111',5,'v');
+INSERT INTO `ticket` (`id_ticket`,`id_evento`,`num_entrada`,`estado`) VALUES ('11111111111111111121','1111111112',1,'v');
+INSERT INTO `ticket` (`id_ticket`,`id_evento`,`num_entrada`,`estado`) VALUES ('11111111111111111122','1111111112',2,'v');
+INSERT INTO `ticket` (`id_ticket`,`id_evento`,`num_entrada`,`estado`) VALUES ('11111111111111111123','1111111112',3,'v');
+INSERT INTO `ticket` (`id_ticket`,`id_evento`,`num_entrada`,`estado`) VALUES ('11111111111111111124','1111111112',4,'v');
+INSERT INTO `ticket` (`id_ticket`,`id_evento`,`num_entrada`,`estado`) VALUES ('11111111111111111125','1111111112',5,'v');
+INSERT INTO `ticket` (`id_ticket`,`id_evento`,`num_entrada`,`estado`) VALUES ('11111111111111111131','1111111113',1,'u');
+INSERT INTO `ticket` (`id_ticket`,`id_evento`,`num_entrada`,`estado`) VALUES ('11111111111111111132','1111111113',2,'u');
+INSERT INTO `ticket` (`id_ticket`,`id_evento`,`num_entrada`,`estado`) VALUES ('11111111111111111133','1111111113',3,'u');
+INSERT INTO `ticket` (`id_ticket`,`id_evento`,`num_entrada`,`estado`) VALUES ('11111111111111111134','1111111113',4,'u');
+INSERT INTO `ticket` (`id_ticket`,`id_evento`,`num_entrada`,`estado`) VALUES ('11111111111111111135','1111111113',5,'u');
+INSERT INTO `ticket` (`id_ticket`,`id_evento`,`num_entrada`,`estado`) VALUES ('11111111111111111141','1111111114',1,'v');
+INSERT INTO `ticket` (`id_ticket`,`id_evento`,`num_entrada`,`estado`) VALUES ('11111111111111111142','1111111114',2,'v');
+INSERT INTO `ticket` (`id_ticket`,`id_evento`,`num_entrada`,`estado`) VALUES ('11111111111111111143','1111111114',3,'v');
+INSERT INTO `ticket` (`id_ticket`,`id_evento`,`num_entrada`,`estado`) VALUES ('11111111111111111144','1111111114',4,'v');
+INSERT INTO `ticket` (`id_ticket`,`id_evento`,`num_entrada`,`estado`) VALUES ('11111111111111111145','1111111114',5,'v');
+INSERT INTO `ticket` (`id_ticket`,`id_evento`,`num_entrada`,`estado`) VALUES ('11111111111111111151','1111111115',1,'v');
+INSERT INTO `ticket` (`id_ticket`,`id_evento`,`num_entrada`,`estado`) VALUES ('11111111111111111152','1111111115',2,'v');
+INSERT INTO `ticket` (`id_ticket`,`id_evento`,`num_entrada`,`estado`) VALUES ('11111111111111111153','1111111115',3,'v');
+INSERT INTO `ticket` (`id_ticket`,`id_evento`,`num_entrada`,`estado`) VALUES ('11111111111111111154','1111111115',4,'v');
+INSERT INTO `ticket` (`id_ticket`,`id_evento`,`num_entrada`,`estado`) VALUES ('11111111111111111155','1111111115',5,'v');
